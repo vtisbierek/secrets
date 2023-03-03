bb021ae2cc5d4f8562741e2c20b6a3726e759146
Victor Tisbierek <metropoller@gmail.com>
2023-03-03 11:04:51 -0300

    including cyclic.sh live url

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ passport.use(new GoogleStrategy(
     {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets",
+        callbackURL: "https://erin-nervous-clownfish.cyclic.app/auth/google/secrets",
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function(accessToken, refreshToken, profile, done) {
@@ -109,7 +109,7 @@ passport.use(new GitHubStrategy(
     {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/github/secrets"
+        callbackURL: "https://erin-nervous-clownfish.cyclic.app/auth/github/secrets"
     },
     function(accessToken, refreshToken, profile, done) {
         User.findOrCreate({ githubId: profile.id }, function (err, user) {
